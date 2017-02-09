4f3787861d7e00b5fb10c8d355f2a1f3db0c484f
lemon <l3moniak2@gmail.com>
2017-02-09 23:34:21 +0100

    Add HowRichYouAre class + specs

---
+++ lib/how_rich_you_are.rb
@@ -0,0 +1,15 @@
+class HowRichYouAre
+  def self.check(amount)
+    if amount < 0
+      'Poor as shit'
+    elsif amount == 0
+      'Homeless bitch'
+    elsif amount > 100
+      'You can buy me a whore'
+    elsif amount > 100000
+      'Almost rich'
+    elsif amount > 1000000
+      'I am wet'
+    end
+  end
+end
---
+++ spec/how_rich_you_are_spec.rb
@@ -0,0 +1,24 @@
+require 'how_rich_you_are'
+
+describe HowRichYouAre do
+  describe ".check" do
+
+    context "below 0" do
+      it "says that are poor" do
+        expect(HowRichYouAre.check(-10)).to eq('Poor as shit')
+      end
+    end
+
+    context "much money" do
+      it "says that you are almost rich" do
+        expect(HowRichYouAre.check(100001)).to eq('Almost rich')
+      end
+    end
+
+    context "rich as fuck" do
+      it "says that she is wet" do
+        expect(HowRichYouAre.check(99999999)).to eq('I am wet')
+      end
+    end
+  end
+end
